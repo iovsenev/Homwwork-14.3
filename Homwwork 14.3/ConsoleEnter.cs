005a399c95b2e345008f783068989cf1e1364b39
Ilya Ovsenev <ovsenev.ilya@yandex.ru>
2023-12-22 14:14:38 +0300

    Добавьте файлы проекта.

---
+++ Homwwork 14.3/ConsoleEnter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homwwork_14._3
+{
+    internal static class ConsoleEnter
+    {
+
+        public static bool CheckExit()
+        {
+            ConsoleColor def = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\nДля выхода введите Exit!");
+            Console.ForegroundColor = def;
+            var input = Console.ReadLine();
+            return input.Equals("Exit", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public static int GetIntNumber(int count)
+        {
+            while (true)
+            {
+                Console.WriteLine("\nЧисло от 1 до {0}", count / 2);
+                int page;
+                var input = Console.ReadLine();
+
+                if (Int32.TryParse(input, out page) && page <= count / 2 && page > 0)
+                    return page;
+
+                ConsoleColor def = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Не верный ввод!");
+                Console.ForegroundColor = def;
+            }
+        }
+    }
+}
---
+++ Homwwork 14.3/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Homwwork_14._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            //  создаём пустой список с типом данных Contact
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            GetBookByPage(phoneBook
+                .OrderBy(c => c.Name)
+                .ThenBy(c => c.LastName));
+        }
+
+        static void GetBookByPage(IEnumerable<Contact> contacts)
+        {
+            while (true)
+            {
+                Console.WriteLine("\nВведите номер страницы!");
+                int pageinput =ConsoleEnter.GetIntNumber(contacts.Count());
+                var skip = (pageinput - 1) * 2;
+                var take = 2;
+                var page = contacts
+                    .Skip(skip)
+                    .Take(take);
+                var i = 1;
+                foreach (var contact in page)
+                {
+                    Console.WriteLine("i: {0}", i);
+                    Console.WriteLine(contact.ToString());
+                    i++;
+                }
+                if (ConsoleEnter.CheckExit())
+                    return;
+            }
+        }
+    }
+}
+
